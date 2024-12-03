0b207eebd6f00322b48705f869200ccd4920f857
mathyslechef <myriamgohoure@gmail.com>
2024-12-03 01:50:40 +0100

    Update streamlit3.py

--- streamlit3.py
+++ streamlit3.py
@@ -26,7 +26,7 @@ authenticator = Authenticate(
 )
 
 # Connexion et gestion du formulaire d'authentification
-st.title("Application de Connexion et d'Album Photo de chats")
+st.title("Bienvenue à l'application de : connectez-vous pour accéder à l'album")
 
 # Formulaire d'authentification
 if authenticator.authenticate():
