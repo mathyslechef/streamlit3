5983ca4b55e1f223b20db1f2add1158650f00c31
mathyslechef <myriamgohoure@gmail.com>
2024-12-03 01:11:31 +0100

    Update streamlit3.py

--- streamlit3.py
+++ streamlit3.py
@@ -1,5 +1,6 @@
 import streamlit as st
 from streamlit_authenticator import Authenticate
+from streamlit_option_menu import option_menu
 
 # Données des comptes utilisateurs
 lesDonneesDesComptes = {
@@ -41,9 +42,25 @@ def accueil():
 
 # Gestion de l'authentification
 if st.session_state["authentication_status"]:
-    accueil()
+    # Menu de navigation
+    selection = option_menu(
+        menu_title=None,  # Aucun titre
+        options=["Accueil", "Photos", "Dashboard"],  # Liste des pages disponibles
+        icons=["house", "image", "bar-chart-line"],  # Icônes associées à chaque page
+        default_index=0  # Page par défaut au démarrage
+    )
+
+    # Affichage du contenu en fonction de la sélection
+    if selection == "Accueil":
+        st.write("Bienvenue sur la page d'accueil!")
+    elif selection == "Photos":
+        st.write("Bienvenue sur la page des photos!")
+    elif selection == "Dashboard":
+        st.write("Bienvenue sur le tableau de bord!")
+
     # Bouton de déconnexion
     authenticator.logout("Déconnexion")
+
 elif st.session_state["authentication_status"] is False:
     st.error("Nom d'utilisateur ou mot de passe incorrect.")
 elif st.session_state["authentication_status"] is None:
