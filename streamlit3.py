9f3533c5ab1e7d7ff2c6333654f23e66332c4e2e
mathyslechef <myriamgohoure@gmail.com>
2024-12-03 02:20:27 +0100

    Update streamlit3.py

--- streamlit3.py
+++ streamlit3.py
@@ -105,4 +105,54 @@ elif selection == "Photos":
 
     st.write("Bienvenue sur mon album photo")
 
-# ... et ainsi de suite pour les autres pages
+import streamlit as st
+
+
+col1, col2, col3 = st.columns(3)
+
+
+with col1:
+
+  st.header("A cat")
+
+  st.image("https://static.streamlit.io/examples/cat.jpg")
+
+
+with col2:
+
+  st.header("A dog")
+
+  st.image("https://static.streamlit.io/examples/dog.jpg")
+
+
+with col3:
+
+  st.header("An owl")
+
+  st.image("https://static.streamlit.io/examples/owl.jpg")
+
+
+# Using object notation
+
+add_selectbox = st.sidebar.selectbox(
+
+    "How would you like to be contacted?",
+
+    ("Email", "Home phone", "Mobile phone")
+
+)
+
+
+# Using "with" notation
+
+with st.sidebar:
+
+    add_radio = st.radio(
+
+        "Choose a shipping method",
+
+        ("Standard (5-15 days)", "Express (2-5 days)")
+
+    )
+
+  
